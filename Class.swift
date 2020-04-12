7e4d4e670881cf9bfae90a1fa6070dd42e09aa56
CoolCoder829 <63531778+CoolCoder829@users.noreply.github.com>
2020-04-11 23:09:36 -0700

    Add files via upload

---
+++ Class.swift
@@ -0,0 +1,16 @@
+//
+//  File.swift
+//  randomstuff
+//
+//  Created by Gavin Wong on 11/23/19.
+//  Copyright © 2019 CodifyAsia. All rights reserved.
+//
+
+import Foundation
+
+struct Class {
+    let classTitle : String
+    let teacher : String
+    let id : String
+    let teacherID: String
+}
---
+++ Message.swift
@@ -0,0 +1,21 @@
+//
+//  Message.swift
+//  ClassroomConnections
+//
+//  Created by Ricky Wang on 11/23/19.
+//  Copyright © 2019 CodifyAsia. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    var sender : String
+    var body : String
+    var senderID : String
+    var messageType : String
+    //var num : Int
+    var ID : Int
+    var correct: Bool
+    var name: String
+    
+}
